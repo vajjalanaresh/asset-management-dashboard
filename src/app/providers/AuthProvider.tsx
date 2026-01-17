1324f7a229499291f89644ec64e46bf6617bd189
cricketbooking03 <cricketbooking03@gmail.com>
2026-01-17 16:37:53 +0530

    fix: changed the env variables naming

--- src/app/providers/AuthProvider.tsx
+++ src/app/providers/AuthProvider.tsx
@@ -3,11 +3,11 @@ import { Auth0Provider } from "@auth0/auth0-react";
 export function AuthProvider({ children }: { children: React.ReactNode }) {
   return (
     <Auth0Provider
-      domain={import.meta.env.AUTH0_DOMAIN}
-      clientId={import.meta.env.AUTH0_CLIENT_ID}
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
       authorizationParams={{
         redirect_uri: window.location.origin,
-        audience: import.meta.env.AUTH0_AUDIENCE,
+        audience: import.meta.env.VITE_AUTH0_AUDIENCE,
       }}
     >
       {children}
